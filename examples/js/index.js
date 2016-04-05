1493ed1f1c28701e822978363f2384ba01caa84d
Ramkumar Shankar <ramkumarshankr@gmail.com>
2016-04-05 22:22:03 +1000

    ported c++ implementation

---
+++ examples/js/index.js
@@ -0,0 +1,90 @@
+var currentTouches = [];
+
+window.onload = function() {
+  var touchCanvas = document.getElementById("canvas");
+  touchCanvas.width = window.innerWidth/2;
+  touchCanvas.height = window.innerHeight/2;
+
+  touchCanvas.addEventListener("touchstart", handleTouchStart, false);
+  touchCanvas.addEventListener("touchend", handleTouchEnd, false);
+  touchCanvas.addEventListener("touchmove", handleTouchMove, false);
+  console.log("touch listeners initialized.");
+
+  runLoop();
+};
+
+function handleTouchStart(evt) {
+  evt.preventDefault();
+  if (currentTouches.length == 1) {
+    return;
+  }
+  for (var i = 0; i < evt.changedTouches.length; i++) {
+    currentTouches.push(copyTouch(evt.changedTouches[i]));
+  }
+  cocSwipe.pointDown(currentTouches[0].pageX, currentTouches[0].pageY);
+}
+
+function handleTouchMove(evt) {
+  evt.preventDefault();
+  for (var i = 0; i < evt.changedTouches.length; i++) {
+    var idx = ongoingTouchIndexById(evt.changedTouches[i].identifier);
+    if (idx >= 0) {
+      cocSwipe.pointMoved(evt.changedTouches[idx].pageX, evt.changedTouches[idx].pageY);
+      // console.log(evt.changedTouches[idx]);
+    }
+    else {
+      //Only handle single touches now
+      console.log("no touch to continue");
+    }
+  }
+}
+
+function handleTouchEnd(evt) {
+  evt.preventDefault();
+  for (var i = 0; i < evt.changedTouches.length; i++) {
+    var idx = ongoingTouchIndexById(evt.changedTouches[i].identifier);
+    if (idx >= 0) {
+      // console.log(evt.changedTouches[idx]);
+      cocSwipe.pointUp(evt.changedTouches[idx].pageX, evt.changedTouches[idx].pageY);
+      currentTouches.splice(idx, 1);
+    }
+    else {
+      //Only handle single touches now
+      console.log("no touch to end");
+    }
+  }
+}
+
+function runLoop() {
+  //Code here
+  cocSwipe.update();
+
+  var canvas = document.getElementById('canvas');
+  var context = canvas.getContext('2d');
+  var radius = 5;
+  for (var i = 0; i < cocSwipe.points.length; i++) {
+    // console.log(cocSwipe.points[i].position);
+    context.fillStyle = 'green';
+    context.beginPath();
+    context.arc(cocSwipe.points[i].position.x, cocSwipe.points[i].position.y, radius, 0, 2 * Math.PI, false);
+    context.fill();
+    context.closePath();
+  }
+
+  requestAnimationFrame(runLoop);
+}
+
+function copyTouch(touch) {
+  return { identifier: touch.identifier, pageX: touch.pageX, pageY: touch.pageY };
+}
+
+function ongoingTouchIndexById(idToFind) {
+  for (var i = 0; i < currentTouches.length; i++) {
+    var id = currentTouches[i].identifier;
+
+    if (id == idToFind) {
+      return i;
+    }
+  }
+  return -1;    // not found
+}
---
+++ src/coc-swipe.js
@@ -0,0 +1,298 @@
+var cocSwipe = cocSwipe || {};
+
+cocSwipe.SwipeDirectionEnum = {
+  UNDEFINED: 0,
+  UP: 1,
+  RIGHT: 2,
+  DOWN: 3,
+  LEFT: 4
+};
+
+cocSwipe.SwipePointTypeEnum = {
+  UNDEFINED: 0,
+  DOWN: 1,
+  MOVED: 2,
+  UP: 3
+};
+
+cocSwipe.points = [];
+cocSwipe.pointsNew = [];
+
+cocSwipe.swipePixelDistanceThreshold = 50;
+cocSwipe.swipePixelVelocityThreshold = 100;
+cocSwipe.swipeTime = '';
+cocSwipe.gestureDirection = cocSwipe.SwipeDirectionEnum.UNDEFINED;
+cocSwipe.gestureStartIndex = -1;
+cocSwipe.bGestureFoundNew = false;
+cocSwipe.time = 0;
+
+cocSwipe.swipePoint = function (xPos, yPos, _type) {
+  return {
+    type: _type,
+    position: {
+      x: xPos,
+      y: yPos
+    },
+    velocity: {
+      x: 0,
+      y: 0
+    },
+    velocityScale: 0,
+    angleDeg: 0,
+    time: 0,
+  };
+};
+
+cocSwipe.setSwipePixelDistanceThreshold = function (value) {
+  this.swipePixelDistanceThreshold = value;
+};
+
+cocSwipe.setSwipePixelVelocityThreshold = function (value) {
+  this.swipePixelVelocityThreshold = value;
+};
+
+cocSwipe.pointUp = function(x, y) {
+  this.pointNew(x, y, this.SwipePointTypeEnum.UP);
+};
+
+cocSwipe.pointDown = function(x, y) {
+  this.pointNew(x, y, this.SwipePointTypeEnum.DOWN);
+};
+
+cocSwipe.pointMoved = function(x, y) {
+  this.pointNew(x, y, this.SwipePointTypeEnum.MOVED);
+};
+
+cocSwipe.pointNew = function(x, y, type) {
+  var point = this.swipePoint(x, y, type);
+  this.pointsNew.push(point);
+};
+
+cocSwipe.reset = function() {
+  this.points = [];
+  this.swipeTime = 0;
+
+  this.gestureDirection = this.SwipeDirectionEnum.UNDEFINED;
+  this.gestureStartIndex = -1;
+};
+
+cocSwipe.getSwipeGestureDirection = function() {
+  return this.gestureDirection;
+};
+
+cocSwipe.getPoints = function() {
+  return this.points;
+};
+
+cocSwipe.getSwipeTime = function() {
+  return this.swipeTime;
+};
+
+cocSwipe.update = function() {
+  this.bGestureFoundNew = false;
+
+  var bSwipeStopped = false;
+
+  if (this.points.length > 0) {
+    if (this.points[this.points.length-1].type == this.SwipePointTypeEnum.UP) {
+      bSwipeStopped = true;
+    }
+    if (bSwipeStopped) {
+      var bDownNew = false;
+      for (var i = 0; i < this.pointsNew.length; i++) {
+        if (this.pointsNew[i].type == this.SwipePointTypeEnum.DOWN) {
+          bDownNew = true;
+          break;
+        }
+      }
+      if (bDownNew) {
+        bSwipeStopped = false;
+        this.reset();
+      }
+    }
+  }
+
+  if (bSwipeStopped) {
+    return;
+  }
+
+  var bSwipeStarted = false;
+  if (this.points.length > 0) {
+    bSwipeStarted = true;
+  }
+
+  var bSwipeStartedNow = false;
+  if (!bSwipeStarted && this.pointsNew.length > 0) {
+    if (this.pointsNew[0].type == this.SwipePointTypeEnum.DOWN) {
+      bSwipeStartedNow = true;
+    }
+  }
+
+  var bUpdate = false;
+  if (bSwipeStarted || bSwipeStartedNow) {
+    bUpdate = true;
+  }
+  else {
+    return;
+  }
+
+  //Update swipe time
+  var now = new Date().getTime();
+  var delta = (now - (this.time || now))/1000;
+  this.time = now;
+
+  if (bSwipeStartedNow) {
+    this.swipeTime = 0;
+  }
+  else {
+    this.swipeTime += delta;
+  }
+
+  if(this.pointsNew.length === 0) {
+    return;
+  }
+
+  var bFound = false;
+  var pointNew;
+
+  if (bSwipeStartedNow) {
+    //look for touch down
+    for (var i = 0; i < this.pointsNew.length; i++) {
+      if (this.pointsNew[i].type === this.SwipePointTypeEnum.DOWN) {
+        pointNew = Object.assign({}, this.pointsNew[i]);
+        bFound = true;
+        break;
+      }
+    }
+  }
+  else {
+    //look for touch up
+    for (var i = this.pointsNew.length - 1; i >= 0; i--) {
+      if (this.pointsNew[i].type === this.SwipePointTypeEnum.UP) {
+        var bSame = false;
+        if (this.points.length > 0) {
+          if (this.pointsNew[i].position.x == this.points[this.points.length-1].position.x && this.pointsNew[i].position.y == this.points[this.points.length-1].position.y) {
+            bSame = true;
+          }
+        }
+        if (bSame) {
+          this.points.splice(this.points.length-1, 1);
+        }
+        pointNew = Object.assign({}, this.pointsNew[i]);
+        bFound = true;
+        break;
+      }
+    }
+    if (!bFound) {
+      for (var i = this.pointsNew.length - 1; i >= 0; i--) {
+        if (this.pointsNew[i].type === this.SwipePointTypeEnum.MOVED) {
+          var bSame = false;
+          if (this.points.length > 0) {
+            if (this.pointsNew[i].position.x == this.points[this.points.length-1].position.x && this.pointsNew[i].position.y == this.points[this.points.length-1].position.y) {
+              bSame = true;
+            }
+          }
+          if (bSame) {
+            continue;
+          }
+          pointNew = Object.assign({}, this.pointsNew[i]);
+          bFound = true;
+          break;
+        }
+      }
+    }
+  }
+
+  this.pointsNew = [];
+  if (!bFound) {
+    return;
+  }
+
+  this.points.push(pointNew);
+  this.points[this.points.length-1].time = this.swipeTime;
+
+  if (this.points.length > 1) {
+    //Find the velocity
+    var point = vec2.fromValues(this.points[this.points.length-1].position.x, this.points[this.points.length-1].position.y);
+    var pointLast = vec2.fromValues(this.points[this.points.length-2].position.x, this.points[this.points.length-2].position.y);
+    var velocity = vec2.create();
+    vec2.sub(velocity, point, pointLast);
+    var timeDelta = this.points[this.points.length-1].time - this.points[this.points.length-2].time;
+    this.points[this.points.length-1].velocity.x = velocity[0]/timeDelta;
+    this.points[this.points.length-1].velocity.y = velocity[1]/timeDelta;
+
+    this.points[this.points.length-1].velocityScale = vec2.length(vec2.fromValues(velocity[0]/timeDelta, velocity[1]/timeDelta)) / this.swipePixelVelocityThreshold;
+    this.points[this.points.length-1].angleDeg = this.angleClockwise(this.points[this.points.length-1].velocity);
+    this.points[this.points.length-1].angleDeg = (this.points[this.points.length-1].angleDeg / Math.PI) * 180.0;
+  }
+
+  if (this.gestureStartIndex == -1) {
+    this.gestureStartIndex = this.points.length-1;
+  }
+
+  var gesturePointStart;
+  var gestureDirectionStart;
+
+  // console.log(this.gestureStartIndex);
+
+  for (var i = this.gestureStartIndex; i < this.points.length; i++) {
+    if (i == this.gestureStartIndex) {
+      gesturePointStart = Object.assign({}, this.points[i]);
+      gestureDirectionStart = this.getDirectionFromAngle(gesturePointStart.angleDeg);
+    }
+
+    // console.log(this.points[i].velocityScale);
+
+    var gesturePoint = Object.assign({}, this.points[i]);
+    // console.log(gesturePoint.velocity);
+    if(gesturePoint.velocityScale < 1.0) {
+      this.gestureDirection = this.SwipeDirectionEnum.UNDEFINED;
+      this.gestureStartIndex = -1;
+      break;
+    }
+
+    var gestureDirectionNew = this.getDirectionFromAngle(gesturePoint.angleDeg);
+    if (gestureDirectionNew != gestureDirectionStart) {
+      this.gestureDirection = this.SwipeDirectionEnum.UNDEFINED;
+      gestureStartIndex = -1;
+      break;
+    }
+
+    var dist = vec2.distance(vec2.fromValues(gesturePoint.position.x, gesturePoint.position.y), vec2.fromValues(gesturePointStart.position.x, gesturePointStart.position.y));
+    if (dist >= this.swipePixelDistanceThreshold) {
+      if (this.gestureDirection != gestureDirectionNew) {
+        this.bGestureFoundNew = true;
+        console.log(gestureDirectionNew);
+      }
+      this.gestureDirection = gestureDirectionNew;
+    }
+  }
+
+
+  //Check if last event is an up event
+};
+
+cocSwipe.angleClockwise = function(direction) {
+  var angle = Math.atan2(direction.y, direction.x);
+  angle += Math.PI * 0.5;
+  if(angle < 0.0) {
+    angle += Math.PI * 2;
+  } else if(angle > Math.PI * 2) {
+    angle -= Math.PI * 2;
+  }
+  return angle;
+};
+
+cocSwipe.getDirectionFromAngle = function(angleDeg) {
+  var dir;
+  if((angleDeg > 315 && angleDeg <= 360) || (angleDeg >= 0 && angleDeg <= 45)) {
+    dir = this.SwipeDirectionEnum.UP;
+  } else if(angleDeg > 45 && angleDeg <= 135) {
+    dir = this.SwipeDirectionEnum.RIGHT;
+  } else if(angleDeg > 135 && angleDeg <= 225) {
+    dir = this.SwipeDirectionEnum.DOWN;
+  } else if(angleDeg > 225 && angleDeg <= 315) {
+    dir = this.SwipeDirectionEnum.LEFT;
+  }
+  return dir;
+};
