e7006a43fcab5651a221999bf32d6b81b12f5329
Ramkumar Shankar <ramkumarshankr@gmail.com>
2016-06-22 15:56:05 +1000

    add licensing information

--- src/coc-swipe.js
+++ src/coc-swipe.js
@@ -1,10 +1,25 @@
-/***************
-coc-swipe.js
-v 1.0
-Ramkumar Shankar
-Port of the C++ cocSwipe class for swipe detection
-Created 06/04/2016
-***************/
+/**
+ *
+ *      ┌─┐╔═╗┌┬┐┌─┐
+ *      │  ║ ║ ││├┤
+ *      └─┘╚═╝─┴┘└─┘
+ *   ┌─┐┌─┐╔╗╔┬  ┬┌─┐┌─┐
+ *   │  ├─┤║║║└┐┌┘├─┤└─┐
+ *   └─┘┴ ┴╝╚╝ └┘ ┴ ┴└─┘
+ *
+ * Copyright (c) 2016 Code on Canvas Pty Ltd, http://CodeOnCanvas.cc
+ *
+ * This software is distributed under the MIT license
+ * https://tldrlegal.com/license/mit-license
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code
+ *
+ **/
+
+//
+// Port of the C++ cocSwipe class for swipe detection
+//
 
 //Specify dependency on vec2 class here
 var vec2 = require('../lib/vec2');
